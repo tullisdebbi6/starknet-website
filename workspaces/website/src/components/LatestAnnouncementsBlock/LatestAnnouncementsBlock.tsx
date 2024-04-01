9139efd2c63d52dbb0f35c91061c02c7b3836092
Tal-Ben-Avi <talb@webiya.co.il>
2024-04-01 11:14:22 +0300

    fixing UI

--- workspaces/website/src/components/LatestAnnouncementsBlock/LatestAnnouncementsBlock.tsx
+++ workspaces/website/src/components/LatestAnnouncementsBlock/LatestAnnouncementsBlock.tsx
@@ -15,7 +15,7 @@ const LatestAnnouncementsBlock = ({ list }: NavbarStickyBannerProps) => {
   return (
     <Box
       position={{ base: "relative", lg: "fixed" }}
-      top={{ base: "unset", lg: "224px" }}
+      top={{ base: "unset", lg: "200px" }}
       right={{ base: "unset", lg: "32px" }}
       width={{ base: "100%", lg: "225px" }}
       mt={{ base: 6, lg: "unset" }}
@@ -26,17 +26,44 @@ const LatestAnnouncementsBlock = ({ list }: NavbarStickyBannerProps) => {
       borderRadius={8}
       zIndex={{ base: "unset", lg: 100 }}
     >
-      <Heading as="h5" fontSize="18px" fontWeight={600} textAlign="left">
+      <Heading
+        as="h5"
+        fontSize={18}
+        fontWeight={600}
+        textAlign="left"
+        mb={3}
+        lineHeight="25px"
+        color="snNavy"
+      >
         Latest announcements
       </Heading>
       <Box display="flex" flexDir={{ base: "row", lg: "column" }} gap={4}>
         {list.map((item) => (
-          <Box mb={6} id={item.buttonLink + item.text}>
-            <Image src={item.image} alt={item.image} borderRadius={8} />
-            <Text fontSize="14px" fontWeight={600} mt={3} mb={2}>
+          <Box key={item.buttonLink + item.text}>
+            <Image
+              src={item.image}
+              alt={item.image}
+              borderRadius={8}
+              width="200px"
+              height="100px"
+            />
+            <Text
+              fontSize={14}
+              mt={3}
+              fontWeight={600}
+              mb={2}
+              color="snNavy"
+              lineHeight="21px"
+            >
               {item.text}
             </Text>
-            <Link href={item.buttonLink} color="Link" onClick={onReadMore}>
+            <Link
+              href={item.buttonLink}
+              color="Link"
+              fontWeight="500"
+              onClick={onReadMore}
+              fontSize={12}
+            >
               {item.buttonText} -{">"}
             </Link>
           </Box>
